77d0472924feb21f302e330cf8d60413cb32b619
chamadoceu <schweigert.fabi@gmail.com>
2025-08-28 20:47:01 -0300

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -3,10 +3,11 @@ const auth = firebase.auth();
 auth.onAuthStateChanged((user) => {
   if (!user) {
     // Se não tiver login → volta pra tela de login
-    window.location.href = "login.html";
+    window.location.href = "index.html";
   }
 });
 document.getElementById("btn-logout")?.addEventListener("click", async () => {
   await auth.signOut();
-  window.location.href = "login.html";
+  window.location.href = "index.html";
+
 });
