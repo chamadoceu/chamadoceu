e866d15b79badfde3f31500d1cedc0290b4b1628
chamadoceu <schweigert.fabi@gmail.com>
2025-08-28 21:02:12 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
-const firebaseConfig = {
+// Inicializa Firebase (modo compat)
+const app = firebase.initializeApp({
   apiKey: "AIzaSyBSj74mcJU3OO2_ZuIg-PtCKgRfyd8QCXo",
   authDomain: "chamadoceu-a8f24.firebaseapp.com",
   projectId: "chamadoceu-a8f24",
@@ -6,5 +7,4 @@ const firebaseConfig = {
   messagingSenderId: "84604132782",
   appId: "1:84604132782:web:f863285b02c5f2441c7c3f",
   measurementId: "G-WB6FXH08LY"
-};
-firebase.initializeApp(firebaseConfig);
+});
